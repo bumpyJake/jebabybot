64b9878a1a6b51a0f4115b900066ccd1c1fc1a51
bumpyJake <bumpyjake@outlook.com>
2020-08-22 20:24:58 +0100

    a

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
-const Canvas = require('canvas');
+
 
 const prefix = "?"
 
@@ -85,64 +85,6 @@ client.on('guildMemberAdd', member => {
 
 
 
-
-// image thing
-
-
-// client.on('guildMemberAdd', async member => {
-// 	const channel = member.guild.channels.cache.find(ch => ch.name === 'member-joins');
-// 	if (!channel) return;
-
-// 	const canvas = Canvas.createCanvas(700, 250);
-// 	const ctx = canvas.getContext('2d');
-
-// 	const background = await Canvas.loadImage('https://i.imgur.com/7rqoEcU.png');
-// 	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-
-// 	ctx.strokeStyle = '#74037b';
-// 	ctx.strokeRect(0, 0, canvas.width, canvas.height);
-
-// 	// Slightly smaller text placed above the member's display name
-// 	ctx.font = '28px Coolvetica';
-// 	ctx.fillStyle = '#ffffff';
-// 	ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 2.2);
-
-//     // Add an exclamation point here and below
-//     // Pass the entire Canvas object because you'll need to access its width, as well its context
-//     const applyText = (canvas, text) => {
-// 	    const ctx = canvas.getContext('2d');
-
-// 	    // Declare a base size of the font
-//     	let fontSize =  70;
-
-// 	    do {
-//         	// Assign the font to the context and decrement it so it can be measured again
-// 	    	ctx.font = `${fontSize -= 10}px Coolvetica`;
-// 	    	// Compare pixel width of the text to the canvas minus the approximate avatar size
-// 	    } while (ctx.measureText(text).width > canvas.width - 300);
-
-//         // Return the result to use in the actual canvas
-        
-
-        
-// 	    return ctx.font;
-//     };
-// 	ctx.font = applyText(canvas, `${member.displayName}!`);
-// 	ctx.fillStyle = '#ffffff';
-// 	ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.6);
-
-// 	ctx.beginPath();
-// 	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
-// 	ctx.closePath();
-// 	ctx.clip();
-
-// 	const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
-// 	ctx.drawImage(avatar, 25, 25, 200, 200);
-
-// 	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
-
-// 	channel.send(attachment);
-// });
 
 
 
